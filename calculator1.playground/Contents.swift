c0a71e5fb707ba61ce31c4d4fd37e63763d2ed70
yurim <yurim830@gmail.com>
2024-03-07 22:49:50 +0900

    계산기 과제

---
+++ calculator1.playground/Contents.swift
@@ -0,0 +1,31 @@
+class Calculator {
+    
+    // 계산 함수 정의
+    func calculate(op:String, _ firstNumber:Double, _ secondNumber:Double) -> Double { // 파라미터명으로 'operator'을 못 쓰는 이유: Swift에서 이미 정의된 키워드라서
+        var result: Double
+        
+        switch op {
+        case "+" :
+            result = firstNumber + secondNumber
+        case "-" :
+            result = firstNumber - secondNumber
+        case "*" :
+            result = firstNumber * secondNumber
+        case "/" :
+            result = firstNumber / secondNumber
+        default: result = 0
+        }
+        
+        return result
+    }
+}
+
+
+// Todo : calculator 변수를 활용하여 사칙연산을 진행
+let calculator = Calculator() // 인스턴스 생성하여 변수에 할당
+let addition = calculator.calculate(op: "+", 10, 3)
+let subtraction = calculator.calculate(op: "-", 10, 3)
+let multiplication = calculator.calculate(op: "*", 10, 3)
+let division = calculator.calculate(op: "/", 10, 3)
+
+
