9b046ea45ff21e5ae571d133035b40b9ad7e5f6c
yurim <yurim830@gmail.com>
2024-03-10 15:48:19 +0900

    변수명 변경 (first, second -> number 1, 2)

--- FinalCalculator/Lv3Calculator.playground/Contents.swift
+++ FinalCalculator/Lv3Calculator.playground/Contents.swift
@@ -18,49 +18,53 @@ class Calculator {
     let mod = ModOperation()
 
     // 2. 동작 (메서드)
-    func operation(_ op: OperatorType, _ first: Double, _ second: Double) -> Double {
+    func operation(_ op: OperatorType, _ number1: Double, _ number2: Double) -> Double {
         switch op {
         case .add:
-            add.addition(first, second)
+            add.addition(number1, number2)
         case .subtract:
-            subtract.subtraction(first, second)
+            subtract.subtraction(number1, number2)
         case .multiply:
-            multiply.multiplication(first, second)
+            multiply.multiplication(number1, number2)
         case .divide:
-            divide.division(first, second)
+            divide.division(number1, number2)
         case .mod:
-            mod.modulo(Int(first), Int(second))
+            mod.modulo(Int(number1), Int(number2))
         }
     }
 }
 
 class AddOperation {
-    func addition(_ first: Double, _ second: Double) -> Double {
-        first + second
+    func addition(_ number1: Double, _ number2: Double) -> Double {
+        number1 + number2
     }
 }
-
+/**
+  - parameters:
+  - number1: 빼기 연산자의 왼쪽에 오는 수
+  - number2: 빼기 연산자의 오른쪽에 오는 수
+ */
 class SubtractOperation {
-    func subtraction(_ first: Double, _ second: Double) -> Double {
-        first - second
+    func subtraction(_ number1: Double, _ number2: Double) -> Double {
+        number1 - number2
     }
 }
 
 class MultiplyOperation {
-    func multiplication(_ first: Double, _ second: Double) -> Double {
-        first * second
+    func multiplication(_ number1: Double, _ number2: Double) -> Double {
+        number1 * number2
     }
 }
 
 class DivideOperation {
-    func division(_ first: Double, _ second: Double) -> Double {
-        first / second
+    func division(_ number1: Double, _ number2: Double) -> Double {
+        number1 / number2
     }
 }
 
 class ModOperation {
-    func modulo(_ first: Int, _ second: Int) -> Double {
-        Double(first % second)
+    func modulo(_ number1: Int, _ number2: Int) -> Double {
+        Double(number1 % number2)
     }
 }
 
