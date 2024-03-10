582ac4b9d49f0f9638613f611149fe1bf97e2f92
yurim <yurim830@gmail.com>
2024-03-10 16:14:20 +0900

    저장

--- FinalCalculator/Lv4Calculator.playground/Contents.swift
+++ FinalCalculator/Lv4Calculator.playground/Contents.swift
@@ -1,4 +1,9 @@
-// 추상화 : 가리고 싶은 것은 가리기
+/* 코드 핵심
+1. 추상화 : 가리고 싶은 것은 가리기
+2. 다형성: 하나의 인스턴스를 여러가지 타입으로 다루게 함
+*/
+
+
 class AbstractOperation {
     func operate(_ first: Double, _ second: Double) -> Double {
         return -1
@@ -48,16 +53,37 @@ class Calculator {
         // 다형성: 하나의 인스턴스를 여러가지 타입으로 다루게 하는 것.
         // AbstractOperation의 하위 클래스 타입도 사용할 수 있음.
 
-    func operation(_ first: Double, _ second: Double) -> Double {
-        operation?.operate(first, second)
+    func operate(_ first: Double, _ second: Double) -> Double {
+        (operation?.operate(first, second))!
     }
 }
 
 
+
+
+
+let addOperation = AddOperation()
+let subtractOperation = SubtractOperation()
+let multiplyOperation = MultiplyOperation()
+let divideOperation = DivideOperation()
+let modOperation = ModOperation()
+
 let calculator = Calculator()
-//calculator.operation(.add, 5, 3)        // 8
-//calculator.operation(.subtract, 5, 3)   // 2
-//calculator.operation(.multiply, 5, 3)   // 15
-//calculator.operation(.divide, 5, 3)     // 1.666666666666667
-//calculator.operation(.mod, 5, 3)        // 2
+
+// 더하기
+calculator.operation = addOperation
+calculator.operate(5, 3) // 8
+
+// 빼기
+calculator.operation = subtractOperation
+calculator.operate(5, 3) // 2
+
+// 곱하기
+calculator.operation = multiplyOperation
+calculator.operate(5,3) // 15
+
+// 나누기
+calculator.operation = divideOperation
+calculator.operate(5,3) // 1.666666666666667
+
 
